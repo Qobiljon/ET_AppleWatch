0663775cdc30b4d7fd7596e629c6a8e822fc9c4e
Qobiljon <qobiljon.toshnazarov@gmail.com>
2020-11-19 03:26:34 +0900

    GRPC dependencies added, data record submission operation implemented with Swift

--- ETAppleIntegration WatchKit Extension/Sensors/ETSensor.swift
+++ ETAppleIntegration WatchKit Extension/Sensors/ETSensor.swift
@@ -1,6 +1,43 @@
 import Foundation
+import GRPC
+import NIO
 
 class ETSensor {
     public static let motion = ETCoreMotion()
     public static let health = ETHealthKit()
+    private static var channel: ClientConnection?
+    
+    private static func getChannel() -> ClientConnection {
+        if ETSensor.channel != nil && ETSensor.channel?.connectivity.state == ConnectivityState.ready {
+            return ETSensor.channel!
+        }
+        
+        let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        defer {
+            try! group.syncShutdownGracefully()
+        }
+        
+        ETSensor.channel = ClientConnection.insecure(group: group).connect(host: "165.246.42.172", port: 50053)
+        return ETSensor.channel!
+    }
+    
+    public static func submitData(dataSourceId: Int32, timestamp: Int64, value: Data) {
+        // get a channel
+        let channel = getChannel()
+        
+        // perform an RPC
+        let client = ETServiceClient(channel: channel)
+        client.submitDataRecord(.with{
+            $0.userID = 1
+            $0.email = "nslabinha@gmail.com"
+            $0.campaignID = 1 // tbd change
+            
+            $0.timestamp = timestamp
+            $0.dataSource = dataSourceId
+            $0.accuracy = 1.0
+            $0.value = value
+        }).response.whenSuccess { response in
+            print("gRPC success \(response.success)")
+        }
+    }
 }
